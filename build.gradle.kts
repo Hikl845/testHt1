b195ccfe8526e17bcdaf6ef496fcb88e7c473973
Hikl845 <freelancer198756@gmail.com>
2026-04-24 00:40:39 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.projectlombok:lombok:1.18.44")
 }
 
 tasks.test {
